60b46f194ef4c2ea405bde626b9964df9c7f0548
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-05-26 10:42:47 +0200

    arima full prediction

--- consumption/full-prediction/models.py
+++ consumption/full-prediction/models.py
@@ -3,15 +3,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class SetpointPredictionMLPModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(SetpointPredictionMLPModel, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, output_size)
+class MLPModel(nn.Module):
+    def __init__(self, input_size, hidden_size_1, hidden_size_2, output_size):
+        super(MLPModel, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size_1)
+        self.fc2 = nn.Linear(hidden_size_1, hidden_size_2)
+        self.fc3 = nn.Linear(hidden_size_2, output_size)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        out = torch.relu(self.fc1(x))
+        out = torch.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
