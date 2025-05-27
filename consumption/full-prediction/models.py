bc35d4986e821bbb2290969323352b6116649a0e
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-05-27 11:31:03 +0200

    add linear regression prediction

--- consumption/full-prediction/models.py
+++ consumption/full-prediction/models.py
@@ -15,3 +15,13 @@ class MLPModel(nn.Module):
         out = torch.relu(self.fc2(out))
         out = self.fc3(out)
         return out
+
+
+class LinearRegressionModel(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(LinearRegressionModel, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
